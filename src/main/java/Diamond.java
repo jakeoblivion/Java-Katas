16bd22aeedaf773f7bd09d4450bd3ff7dfdd0263
jakeoblivion <jakeoblivion@gmail.com>
2017-05-19 23:37:48 +0100

    created diamond class and initial test

--- src/main/java/Diamond.java
+++ src/main/java/Diamond.java
@@ -1,3 +1,25 @@
 public class Diamond {
+    public void drawIsoscelesTriangle(int triangleHeight) {
+        for(int x = 0; x < triangleHeight; x++) {
+            drawSpace();
+            drawSpace();
+            drawAsterisk();
+            drawSpace();
+            drawSpace();
+            drawNewLine();
+        }
+    }
+
+    public void drawAsterisk() {
+        System.out.print('*');
+    }
+
+    public void drawSpace() {
+        System.out.print(' ');
+    }
+
+    public void drawNewLine() {
+        System.out.println();
+    }
 
 }
--- src/test/java/DiamondTest.java
+++ src/test/java/DiamondTest.java
@@ -1,3 +1,43 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static com.sun.jmx.snmp.ThreadContext.contains;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
 public class DiamondTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+
+    @Test
+    public void drawIsoscelesTriangleFirstLine() throws Exception {
+        Diamond diamond = new Diamond();
+        diamond.drawIsoscelesTriangle(3);
+        assertThat(outContent.toString(), containsString("  *  " ));
+    }
+/*
+
+    @Test
+    public void drawIsoscelesTriangle() throws Exception {
+        Diamond diamond = new Diamond();
+        diamond.drawIsoscelesTriangle(3);
+        assertEquals("  *  \n *** \n*****", outContent.toString());
+    }
+*/
 
 }
