dfa467bbbd7e5f75ec45047971699c07dc3fc35d
jakeoblivion <jakeoblivion@gmail.com>
2017-05-21 15:59:17 +0100

    refactored prime factors

--- src/main/java/PrimeFactors.java
+++ src/main/java/PrimeFactors.java
@@ -2,14 +2,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactors {
-    List<Integer> primes = new ArrayList<>();
+    List<Integer> primeFactors = new ArrayList<>();
 
     public List<Integer> generatePrimeFactors(int n) {
-        if (n<=1) {
-
-        } else {
-            primes.add(n);
+        if(n>1) {
+            for(int i = 2;i <= n; i++) {
+                while(i % n == 0) {
+                    i /= n;
+                    primeFactors.add(i);
+                }
+            }
         }
-        return primes;
+        return primeFactors;
     }
 }
--- src/test/java/PrimeFactorsTest.java
+++ src/test/java/PrimeFactorsTest.java
@@ -11,11 +11,87 @@ public class PrimeFactorsTest {
     public void generatePrimeFactorsReturn1() throws Exception {
         PrimeFactors primeFactors = new PrimeFactors();
         List<Integer> primes = primeFactors.generatePrimeFactors(1);
-
         List<Integer> expectedList = Collections.emptyList();
 
         assertEquals(expectedList,primes);
     }
 
+    @Test
+    public void generatePrimeFactorsReturn2() throws Exception {
+        PrimeFactors primeFactors = new PrimeFactors();
+        List<Integer> primes = primeFactors.generatePrimeFactors(2);
+        List<Integer> expectedList = new ArrayList<>();
+        Collections.addAll(expectedList, 2);
 
+        assertEquals(expectedList,primes);
+    }
+
+    @Test
+    public void generatePrimeFactorsReturn2From4() throws Exception {
+        PrimeFactors primeFactors = new PrimeFactors();
+        List<Integer> primes = primeFactors.generatePrimeFactors(4);
+        List<Integer> expectedList = new ArrayList<>();
+        Collections.addAll(expectedList, 2);
+
+        assertEquals(expectedList,primes);
+    }
+
+    @Test
+    public void generatePrimeFactorsReturn25From10() throws Exception {
+        PrimeFactors primeFactors = new PrimeFactors();
+        List<Integer> primes = primeFactors.generatePrimeFactors(10);
+        List<Integer> expectedList = new ArrayList<>();
+        Collections.addAll(expectedList, 2,5);
+
+        assertEquals(expectedList,primes);
+    }
+
+    @Test
+    public void generatePrimeFactorsReturn27From14() throws Exception {
+        PrimeFactors primeFactors = new PrimeFactors();
+        List<Integer> primes = primeFactors.generatePrimeFactors(14);
+        List<Integer> expectedList = new ArrayList<>();
+        Collections.addAll(expectedList, 2,7);
+
+        assertEquals(expectedList,primes);
+    }
+
+    @Test
+    public void generatePrimeFactorsReturn25From27() throws Exception {
+        PrimeFactors primeFactors = new PrimeFactors();
+        List<Integer> primes = primeFactors.generatePrimeFactors(20);
+        List<Integer> expectedList = new ArrayList<>();
+        Collections.addAll(expectedList, 2,5);
+
+        assertEquals(expectedList,primes);
+    }
+
+    @Test
+    public void generatePrimeFactorsReturn235From30() throws Exception {
+        PrimeFactors primeFactors = new PrimeFactors();
+        List<Integer> primes = primeFactors.generatePrimeFactors(30);
+        List<Integer> expectedList = new ArrayList<>();
+        Collections.addAll(expectedList, 2,3,5);
+
+        assertEquals(expectedList,primes);
+    }
+
+    @Test
+    public void generatePrimeFactorsReturn211From22() throws Exception {
+        PrimeFactors primeFactors = new PrimeFactors();
+        List<Integer> primes = primeFactors.generatePrimeFactors(22);
+        List<Integer> expectedList = new ArrayList<>();
+        Collections.addAll(expectedList, 2,11);
+
+        assertEquals(expectedList,primes);
+    }
+
+    public void generatePrimeFactorsReturn11From121() throws Exception {
+        PrimeFactors primeFactors = new PrimeFactors();
+        List<Integer> primes = primeFactors.generatePrimeFactors(11);
+        List<Integer> expectedList = new ArrayList<>();
+        Collections.addAll(expectedList, 11);
+
+        assertEquals(expectedList,primes);
+    }
 }
